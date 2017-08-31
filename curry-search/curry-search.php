438afcc4cf3610d4e4657a2127b5924ae77cec34
Jakob Demler <jdemler@curry-software.com>
2017-08-31 10:22:36 +0200

    remove beta from description

--- curry-search/curry-search.php
+++ curry-search/curry-search.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: CurrySearch
 Plugin URI:  https://github.com/CurrySoftware/CurrySearch-WordPress
-Description: BETA! Interface to the CurrySearch System. Provides fast and reliable results with autocompletion and tag filters.
+Description: Interface to the CurrySearch System. Provides fast and reliable results with autocompletion and tag filters.
 Version:     1.0
 Author:      CurrySoftware GmbH
 Author URI:  https://www.curry-software.com
