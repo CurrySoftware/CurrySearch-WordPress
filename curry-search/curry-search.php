7568889d80fec80b93c293469af084cb5a2efc82
Jakob Demler <jdemler@curry-software.com>
2017-09-11 18:19:31 +0200

    translation of metadata

--- curry-search/curry-search.php
+++ curry-search/curry-search.php
@@ -5,7 +5,7 @@ Plugin URI:  https://github.com/CurrySoftware/CurrySearch-WordPress
 Description: CurrySearch is an better cloud-based search for WordPress. It supports custom post types, advanced autocomplete, relevance based results and filter.
 Version:     1.0.3
 Author:      CurrySoftware GmbH
-Author URI:  https://www.curry-software.com
+Author URI:  https://www.curry-software.com/en/
 Text Domain: currysearch
 Domain Path: /languages
 License:     GPL3
